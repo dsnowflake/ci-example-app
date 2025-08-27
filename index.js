b9b419b353619ae6a3ca458aba57dbb38bc2599a
dsnowflake <sadrul.email3@gmail.com>
2025-08-27 13:08:08 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@
  * @param {number} b The second number.
  * @returns {number} The sum of the two numbers.
  */
-function add(a, b) {
+function add(a, c) {
   // A simple function to be tested
-  return a + b;
+  return a + c;
 }
 //test
 
@@ -15,5 +15,6 @@ module.exports = {
   add,
 
 };
+
 
 
